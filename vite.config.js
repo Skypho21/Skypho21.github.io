e14a86696d0675b9859c0df815aa8026c555d13f
skyfa <skinthav@millersville.edu>
2024-10-24 14:57:15 -0400

    trying to fix package issues

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ['@ark-ui/react'],
+      external: [
+        '@ark-ui/react/dist/components/file-upload/file-upload-clear-trigger',
+        '@ark-ui/react/dist/components/fieldset/use-fieldset-context',
+        '@ark-ui/react/dist/components/file-upload/file-upload-context',
+        // Add other specific sub-modules that are causing issues
+      ],
     },
   },
 });
