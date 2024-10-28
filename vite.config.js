7ed38f302b084343240aca354534b97cc2e94bb5
skyfa <skinthav@millersville.edu>
2024-10-28 15:07:28 -0400

    changed to vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/skypho21.github.io/',  // Replace with your GitHub repo name
   build: {
     outDir: 'dist',
     sourcemap: false,
